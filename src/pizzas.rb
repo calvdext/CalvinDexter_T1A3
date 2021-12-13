1e8c77fd7b1a41867f32dc9a79fae12878a9e2f7
calvdext <gcab022117@coderacademy.edu.au>
2021-12-13 10:08:57 +1000

    adding a class of Pizzas to create a seed file with all pizza info

---
+++ src/pizzas.rb
@@ -0,0 +1,12 @@
+class Pizzas
+
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+
+    def print_pizzas
+        return "#{@name}: $#{price}"
+    end
+    
+end
