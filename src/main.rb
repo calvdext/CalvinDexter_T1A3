8e4d7e12902321260e526a2c1735b02da1888047
calvdext <gcab022117@coderacademy.edu.au>
2021-12-13 10:02:35 +1000

    require tty-prompt gem creating a main menu of options

--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,24 @@
-puts "Hello World"
+require "tty-prompt"
+
+system "clear"
+$prompt = TTY::Prompt.new
+
+def main_menu
+    awsner = $prompt.select("Main Menu", ["Pizza Menu: ", "Sides Menu: ", "Drinks Menu: ", "My Order: ", "Exit App: "]) 
+    return awsner
+end
+
+options = ""
+while options != "Exit App: "
+    options = main_menu
+    case options
+    when "Pizza Menu: "
+        puts "Select Pizza Menu"
+    when "Sides Menu: "
+        puts " Sides Menu"
+    when "Drinks Menu: "
+        puts "Drinks Menu: "
+    when "My Order: "
+        print "New Order: "
+    end
+end
